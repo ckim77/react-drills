4ca77070d55261887abb0b259e6a14b650a568ed
Chris Kim <ckjr92@gmail.com>
2021-12-14 12:51:09 -0700

    finished app 8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      lebron: ""
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://www.balldontlie.io/api/v1/players/237").then(res => {
+      this.setState ({
+        lebron: res.data
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className = "App">
+        <h1> First Name: {this.state.lebron.first_name} </h1>
+        <h1> Last Name: {this.state.lebron.last_name} </h1>
+        <h1> Position: {this.state.lebron.position} </h1>
+        <h1> Weight: {this.state.lebron.weight_pounds} </h1>
+
+      </div>
+    )
+  }
 }
 
 export default App;
